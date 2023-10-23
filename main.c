6a20a2b4f2beae189ef409d6efd8eddff3689ad6
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2023-10-23 21:57:08 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int n;
+    printf("nhap n= ");
+    scanf("%d",&n);
+    int S=0;
+
+
+    for (int  i = 0;  i <= n; i++ )
+        S = S+ i*i;
+
+    printf("tong binh phuong tu 0 den %d la : %d",n, S);
+
+    }
+
+
