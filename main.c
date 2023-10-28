f5d3d15b0d4ded480e18ca338b51de54e822d393
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2023-10-28 21:13:27 +0700

    BTTH3. bài 1

--- main.c
+++ main.c
@@ -1,25 +1,42 @@
-//Giai phuong trinh an x bac 2
 #include <stdio.h>
-#include <math.h>
-int main () {
-    int a, b ,c ,x;
-    float x0, x1, x2;
-    float delta;
-    printf("nhap a, b ,c\n");
-    scanf("%d%d%d", &a, &b, &c);
-    delta = b*b - 4 *a*c;
-    if (delta == 0) {
-        x0 = -b/2*a;
-        printf("phuong trinh co nghiem kep x= %.2f ", x0);
 
-    } else if (delta < 0){
-        printf("phuong trinh vo nghiem");
-    } else if (delta >0) {
-        x1 = (-b+sqrt(delta))/2*a;
-        x2 = (-b-sqrt(delta)/2*a) ;
-        printf("phuong trinh co 2 nghiem phan biet x1= %.2f , x2 = %.2f : ", x1 , x2);
-
+int main() {
+    long long a;
+    printf("nhap gia tri cua a:");
+    scanf("%lld", &a);
+    if ((-10^18<=a<=10^18)&&(a%5==0)&&(a%3==0)){
+        printf("1\n");
+    } else {
+        printf("0\n");
     }
-
-    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
