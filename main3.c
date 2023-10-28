70741b10b3260f6d941aa65cdb40639e514281ed
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2023-10-29 04:06:08 +0700

    BTTH3. HW3

---
+++ main3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 2 x %d= %d\n", i , 2*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 3 x %d= %d\n", i , 3*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 4 x %d= %d\n", i , 4*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 5 x %d= %d\n", i , 5*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 6 x %d= %d\n", i , 6*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 7 x %d= %d\n", i , 7*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 8 x %d= %d\n", i , 8*i);
+
+    } for (i=1; i<=10; i++) {
+        printf("bang cuu chuong 9 x %d= %d\n", i , 9*i);
+
+    }
+
+    return 0;
+}
