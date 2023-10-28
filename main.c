789a60604bf886246c6bff3981b4dfd1df36d4b6
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2023-10-28 20:57:45 +0700

    Giai phuong trinh bac 2 an x

--- main.c
+++ main.c
@@ -1,18 +1,25 @@
+//Giai phuong trinh an x bac 2
 #include <stdio.h>
 #include <math.h>
-int main() {
-    int n;
-    printf("nhap n= ");
-    scanf("%d",&n);
-    int S=0;
+int main () {
+    int a, b ,c ,x;
+    float x0, x1, x2;
+    float delta;
+    printf("nhap a, b ,c\n");
+    scanf("%d%d%d", &a, &b, &c);
+    delta = b*b - 4 *a*c;
+    if (delta == 0) {
+        x0 = -b/2*a;
+        printf("phuong trinh co nghiem kep x= %.2f ", x0);
 
-
-    for (int  i = 0;  i <= n; i++ )
-        S = S+ i*i;
-
-    printf("tong binh phuong tu 0 den %d la : %d",n, S);
-    return 0;
+    } else if (delta < 0){
+        printf("phuong trinh vo nghiem");
+    } else if (delta >0) {
+        x1 = (-b+sqrt(delta))/2*a;
+        x2 = (-b-sqrt(delta)/2*a) ;
+        printf("phuong trinh co 2 nghiem phan biet x1= %.2f , x2 = %.2f : ", x1 , x2);
 
     }
 
-
+    return 0;
+}
