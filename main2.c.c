81e3c2b18e7d96d8d9cac6015333a4cfb2ed95c1
BachAn1205 <148757605+BachAn1205@users.noreply.github.com>
2023-10-28 21:25:14 +0700

    BTTH3. Bài 2

---
+++ main2.c.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    long long a;
+    printf(" nhap a:");
+    // -10^6 <= a <= 10^6
+    scanf("%lld",&a);
+    if (( a <= 0)) {
+        printf("INVALID");
+    } else if((a%400==0) || (a%4==0) && (a % 100!=0)) {
+        printf("YES");
+    } else {
+        printf("NO");
+    }
+
+    return 0;
+}
+
+
+
